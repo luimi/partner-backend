3dee9eeeb4c924ace225d558c4867c6689d88dab
Luis Meza <luimi@live.com>
2025-09-02 22:01:58 -0500

    agregado cors

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const ParseServer = require('parse-server').ParseServer;
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const { PORT, PARSE_MONGODB_URI, PARSE_APPID, PARSE_MASTERKEY, PARSE_RESTKEY, PARSE_JSKEY, PARSE_SERVER_URL } = process.env;
 
 const server = new ParseServer({
